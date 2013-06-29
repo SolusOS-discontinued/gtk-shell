9107b2b853b6a703f6b4485b4a6e3e10edbc9b04
Ikey Doherty <ikey@solusos.com>
2013-06-29 18:55:58 +0100

    Privatise functions and import code from gtk-shell

---
+++ shell/consort-shell-private.h
@@ -0,0 +1,87 @@
+#ifndef __CONSORT_SHELL_PRIVATE_H__
+#define __CONSORT_SHELL_PRIVATE_H__
+
+#include "wayland-consort-shell-client-protocol.h"
+
+/* Following functions/etc are ripped from gtk-shell.c */
+struct element {
+	GtkWidget *window;
+	GdkPixbuf *pixbuf;
+	struct wl_surface *surface;
+};
+
+struct desktop {
+	struct wl_display *display;
+	struct wl_registry *registry;
+	struct desktop_shell *shell;
+	struct wl_output *output;
+
+	GdkDisplay *gdk_display;
+
+	struct element *background;
+	struct element *panel;
+};
+    
+static void
+desktop_shell_configure(void *data,
+		struct desktop_shell *desktop_shell,
+		uint32_t edges,
+		struct wl_surface *surface,
+		int32_t width, int32_t height)
+{
+	struct desktop *desktop = data;
+
+	gtk_widget_set_size_request (desktop->background->window,
+				     width, height);
+
+	gtk_widget_set_size_request (desktop->panel->window, width, 32);
+}
+
+static void
+desktop_shell_prepare_lock_surface(void *data,
+		struct desktop_shell *desktop_shell)
+{
+}
+
+static void
+desktop_shell_grab_cursor(void *data, struct desktop_shell *desktop_shell,
+		uint32_t cursor)
+{
+}
+
+static const struct desktop_shell_listener listener = {
+	desktop_shell_configure,
+	desktop_shell_prepare_lock_surface,
+	desktop_shell_grab_cursor
+};
+
+static void
+registry_handle_global(void *data, struct wl_registry *registry,
+		uint32_t name, const char *interface, uint32_t version)
+{
+	struct desktop *d = data;
+
+	if (!strcmp(interface, "desktop_shell")) {
+		d->shell = wl_registry_bind(registry, name,
+				&desktop_shell_interface, 1);
+		desktop_shell_add_listener(d->shell, &listener, d);
+	} else if (!strcmp(interface, "wl_output")) {
+
+		/* TODO: create multiple outputs */
+		d->output = wl_registry_bind(registry, name,
+					     &wl_output_interface, 1);
+	}
+}
+
+static void
+registry_handle_global_remove(void *data, struct wl_registry *registry,
+		uint32_t name)
+{
+}
+
+static const struct wl_registry_listener registry_listener = {
+	registry_handle_global,
+	registry_handle_global_remove
+};
+
+#endif /* __CONSORT_SHELL_PRIVATE_H__ */
--- shell/consort-shell.c
+++ shell/consort-shell.c
@@ -1,18 +1,48 @@
+#include <gtk/gtk.h>
+#include <gdk/gdkwayland.h>
+
 #include "consort-shell.h"
+#include "consort-shell-private.h"
 
 G_DEFINE_TYPE (ConsortShell, consort_shell, G_TYPE_OBJECT)
 
 #define CONSORT_SHELL_GET_PRIVATE(o) (G_TYPE_INSTANCE_GET_PRIVATE ((o), CONSORT_SHELL_TYPE, ConsortShellPrivate))
 
 struct _ConsortShellPrivate {
-    gchar *name; /* Reserved right now */
+    gchar           *name; /* Reserved right now */
+    
+    struct desktop  *desktop;
+    GSettings       *background_settings;
 };
 
 
+/* GObject methods */
+
 static void consort_shell_init (ConsortShell *object) {
     ConsortShellPrivate *priv = CONSORT_SHELL_GET_PRIVATE (object);
+    struct desktop *desktop;
     
-    /* Actually init our self */
+    desktop = priv->desktop;
+    desktop = malloc(sizeof *desktop);
+    desktop->output = NULL;
+    desktop->shell = NULL;
+
+    desktop->gdk_display = gdk_display_get_default();
+    desktop->display =
+        gdk_wayland_display_get_wl_display(desktop->gdk_display);
+    if (desktop->display == NULL) {
+        fprintf(stderr, "failed to get display: %m\n");
+        return -1;
+    }
+
+    desktop->registry = wl_display_get_registry(desktop->display);
+    wl_registry_add_listener(desktop->registry,
+            &registry_listener, desktop);
+
+    /* Wait until we have been notified about the compositor and shell
+     * objects */
+    while (!desktop->output || !desktop->shell)
+        wl_display_roundtrip (desktop->display);
 }
 
 static void consort_shell_finalize (GObject *object) {
@@ -23,11 +53,15 @@ static void consort_shell_finalize (GObject *object) {
         g_free (priv->name);
     }
     
+    if (priv->desktop) {
+        free (priv->desktop);
+        priv->desktop = NULL;
+    }
+    
     G_OBJECT_CLASS (consort_shell_parent_class)->finalize (object);
 }
 
-static void
-consort_shell_class_init (ConsortShellClass *klass) {
+static void consort_shell_class_init (ConsortShellClass *klass) {
     GObjectClass *object_class = G_OBJECT_CLASS (klass);
 
     object_class->finalize = consort_shell_finalize;
@@ -47,7 +81,8 @@ ConsortShell* consort_shell_new () {
     return shell;
 }
 
-int main (int argc, char **argv) {
+int main (int argc, char *argv[]) {
+    
     ConsortShell *shell;
     GOptionContext *ctx;
     
@@ -60,8 +95,10 @@ int main (int argc, char **argv) {
         g_print ("consort-shell: %s\n", error->message);
         return 1;
     }
-        
+    gtk_init(&argc, &argv);
+
     shell = consort_shell_new ();
     
+    gtk_main ();
     return 0;
 }
